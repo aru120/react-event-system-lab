03d982fa6a15fe59b35e42174a3f13f9f04c9fc3
aru120 <khuang159@gmail.com>
2020-12-09 17:31:07 -0500

    finished lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe(){
+    return(
+        <button onBlur={onBlur} onFocus={onFocus}></button>
+    )
+}
+
+const onBlur = () =>{
+    console.log("Hey! Eyes on me!")
+}
+
+const onFocus = () =>{
+    console.log("Good!")
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function KeyPad(){
+    return <input type="password" onKeyUp={handleKeyUp} />
+}
+
+const handleKeyUp = () =>{
+    console.log("Entering password...")
+}
+
+export default KeyPad
